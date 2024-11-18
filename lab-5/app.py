e37eb50443daf2ed5249f40b4d09d8c771f77a41
Petrakowww <Petrakov.Egor.G@yandex.ru>
2024-11-18 22:42:51 +0300

    fix app

--- lab-5/app.py
+++ lab-5/app.py
@@ -31,11 +31,11 @@ class User(db.Model):
 
 @app.route('/users', methods=['POST'])
 def create_user():
-	data = request.get_json()
-	new_user = User(username=data['username'], email=data['email'])
-	db.session.add(new_user)
-	db.session.commit()
-	return jsonify({'message': 'User created successfully'})
+    data = request.get_json()
+    new_user = User(username=data['username'], email=data['email'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User created successfully', 'id': new_user.id})
 
 
 @app.route('/data')
